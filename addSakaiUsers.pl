4ab07ccc25b85cd84e0a4fec36406c6c5d149879
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-03-27 17:30:30 +0000

    CT-507: add utility to add test users via webservices.
    git-svn-id: https://source.sakaiproject.org/svn/branches/ctools-winter-06/tools@7060 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- addSakaiUsers.pl
+++ addSakaiUsers.pl
@@ -2,6 +2,8 @@
 #
 # $HeadURL$
 # $Id$
+#
+#
 # This code was adapted from a script provided by Seth Theriault form Columbia.
 #
 # Copyright (c) 2005 The Trustees of Columbia University in the City of New York
@@ -35,90 +37,112 @@ use SOAP::Lite;
 
 use strict;
 
+use addSakaiUsers;
+
+my $trace = 0;
+
 # Variables to configure
-my $session = "";
-my $newuser ="testu7";		# name of user to be created
-my $usertodelete = "";		# name of user to be deleted	
-my $newsiteid ="genchem1";   # unique string to identify site (siteid)
-my $newpagename = "testpage";	# title of new page
-my $deletesiteid = "";		# siteid of site to be deleted
+#my $session = "";
+#my $newuser ="testu7";		# name of user to be created
+#my $usertodelete = "";		# name of user to be deleted	
+#my $newsiteid ="genchem1";   # unique string to identify site (siteid)
+#my $newpagename = "testpage";	# title of new page
+#my $deletesiteid = "";		# siteid of site to be deleted
 
-my $soap2;			# hold soap connection to sakai
+#my $soap2;			# hold soap connection to sakai
 
-my $true = SOAP::Data->value('true')->type('boolean');
-my $false = SOAP::Data->value('')->type('boolean');
+#my $true = SOAP::Data->value('true')->type('boolean');
+#my $false = SOAP::Data->value('')->type('boolean');
 
 # Change these variables as appropriate
-my $loginURI = "http://localhost:8080/sakai-axis/SakaiLogin.jws?wsdl";
-my $scriptURI = "http://localhost:8080/sakai-axis/SakaiScript.jws?wsdl";
-y
+#my $loginURI = "http://localhost:8080/sakai-axis/SakaiLogin.jws?wsdl";
+#my $scriptURI = "http://localhost:8080/sakai-axis/SakaiScript.jws?wsdl";
+
 # Sakai admin user's credentials
 my $user = "admin";
 my $password = "admin";
 
 my $soap;
 
+my $lineCnt;
+
 ### If there is nothing to do, exit.
 
 
 ### Start a Sakai session
 
-sub startSakaiSession {
+# sub startSakaiSession {
 
-  print "Starting a Sakai session...\n";
+#   print "Starting a Sakai session...\n";
 
-  print "Sakai session id (after login) -> ";
-  $soap = SOAP::Lite
-    -> proxy($loginURI)
-      -> uri($loginURI)
-	;
+#   print "Sakai session id (after login) -> ";
+#   $soap = SOAP::Lite
+#     -> proxy($loginURI)
+#       -> uri($loginURI)
+# 	;
 
-  $session = $soap->login($user,$password)->result;
+#   $session = $soap->login($user,$password)->result;
 
-  print "$session\n";
+#   print "$session\n";
 
-}
+# }
 
 ### Connect and futz
 
-sub getSoapConnection {
+# sub getSoapConnection {
 
-  $soap2 = SOAP::Lite
-    -> proxy($scriptURI)
-      -> uri($scriptURI)
-	;
-}
+#   $soap2 = SOAP::Lite
+#     -> proxy($scriptURI)
+#       -> uri($scriptURI)
+# 	;
+# }
 ### Create a user (no session required)
 ###
 ### addNewUser( String sessionid, String userid, String firstname, String lastname, String email, String type, String password)
 
+# my ($userCnt,$sakaiErrorCnt,$soapErrorCnt,$lineCnt,$tries) = (0,0,0,0,0);
 
-sub createUser {
-  my($newuser,$p) = @_;
-  my $result;
-  if ($newuser) {
-    print "\nCreating user \"$newuser\" --> ";
-    # print "s: $session\n";
-    $result = $soap2->addNewUser(
-				 $session,
-				 $newuser,
-				 "Test",
-				 "User",
-				 "$newuser\@somewhere.edu",
-				 "registered",
-				 $p);
-    #    print "r: $result\n";
-    print $result->result;
-    if ($result->fault) {
-      print join(", ",$result->faultcode,$result->faultstring),"\n";
-    }
+# sub createUser {
+#   my($newuser,$p) = @_;
+#   my $result;
+#   if ($newuser) {
+#     $tries++;
+#     print "line: $.: Creating user \"$newuser\" --> ";
+#     # print "s: $session\n";
+#     $result = $soap2->addNewUser(
+# 				 $session,
+# 				 $newuser,
+# 				 "Test",
+# 				 "User",
+# 				 "$newuser\@somewhere.edu",
+# 				 "registered",
+# 				 $p);
+#     #    print "r: $result\n";
+#     print $result->result,"\n";
+# #    print "fault: ",$result->fault,"\n";
+#     if ($result->fault) {
+# #      print "line: $.: user: [$newuser] ";
+#       print join(", ",$result->faultcode,$result->faultstring);
+#       print "\n";
+#       $soapErrorCnt++;
+#     } elsif ($result->result =~ /exception/i) {
+#  #     print "line: $.: user: [$newuser] : sakai exception";
 
-  }
-}
+#       $sakaiErrorCnt++;
+#     } else {
+#       $userCnt++;
+#     }
+#   }
+# #  print "\n";
+# }
 
-our @test = ("bobY acorn","blueY justcorn");
+our @test = (
+	     "AAbobZB acorn",
+	     "AAblueZB justcorn",
+	     "AAblueZAB justcorn"
+	    );
 
-sub main {
+sub testAdd {
   my($u);
   my($user,$pw);
   startSakaiSession();
@@ -126,11 +150,40 @@ sub main {
 
   foreach $u (@test) {
     ($user,$pw) = $u =~ m/(.*)\s+(.*)/;
+    print "u: $user p: $pw\n" if ($trace);
     createUser($user,$pw);
   }
 }
 
+# read in user / pw from file and then add to the 
+sub readFromStdIn {
+  my($user,$pw);
+#  print "call readFromStdIn\n";
+  startSakaiSession();
+  getSoapConnection();
 
+  while (<>) {
+    next if (/^\s*$./);
+    chomp;
+    $lineCnt++;
+#    print "line: $.: [$_]\n";
+    ($user,$pw) = m/(\w*)\s+(\w*)/;
+#    print "user: [$user] pw: [$pw]\n";
+    createUser($user,$pw);
+  }
+  
+}
+
+# sub printSummary {
+#   print "\n";
+#   print "lines: $lineCnt add attempts: $tries added: $userCnt soap errors: $soapErrorCnt sakai errors: $sakaiErrorCnt\n";
+# }
+
+sub main {
+  testAdd();
+#  readFromStdIn();
+  printSummary();
+}
 
 #######
 
