b545fd651ba076914b374eeacb9c12fbd1085a6f
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-07-27 13:25:49 +0000

    CTools: update 2.1.2 -> 2.2.0 conversion scripts.
    git-svn-id: https://source.sakaiproject.org/svn/branches/ctools-summer-06@13019 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- db-conversion/2-2-0/ctools_oracle_conversion_gradebook.sql
+++ db-conversion/2-2-0/ctools_oracle_conversion_gradebook.sql
@@ -1,11 +1,11 @@
 -- Assembled 2006/07/20.
 
--- $HeadURL:$
--- $Id:$
+-- $HeadURL$
+-- $Id$
 
 -- Gradebook related tables changes needed between Sakai 2.01 and 2.1
--- alter table GB_GRADABLE_OBJECT_T add (NOT_COUNTED number(1,0));
--- update GB_GRADABLE_OBJECT_T set NOT_COUNTED=0 where NOT_COUNTED is NULL and POINTS_POSSIBLE is not NULL;
+alter table GB_GRADABLE_OBJECT_T add (NOT_COUNTED number(1,0));
+update GB_GRADABLE_OBJECT_T set NOT_COUNTED=0 where NOT_COUNTED is NULL and POINTS_POSSIBLE is not NULL;
 
 -- Gradebook table changes between Sakai 2.1.* and 2.2.
 
