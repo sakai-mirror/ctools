8b36e7f32935c0e9911b5ff11f46eb45b6cbb367
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-08-17 14:31:50 +0000

    CTools: 2.2.0 2.2.1 conversion script.
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@13817 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- builds/ctools_2-2/db-conversion/2-2-1/sakai_2-2_0-2_2_1_oracle_conversion.sql
+++ builds/ctools_2-2/db-conversion/2-2-1/sakai_2-2_0-2_2_1_oracle_conversion.sql
@@ -1,4 +1,9 @@
 -- This is the Oracle Sakai 2.2.0 -> 2.2.1 conversion script
+
+-- This version annotated for UMich.
+-- $HeadURL:$
+-- $Id:$
+
 ----------------------------------------------------------------------------------------------------------------------------------------
 --
 -- use this to convert a Sakai database from 2.2.0 to 2.2.1.  Run this before you run your first app server.
@@ -10,6 +15,8 @@
 -- http://bugs.osportfolio.org/jira/browse/OSP-1607
 -- Increasing the size of fields that hold a site id to 99.  Some were not specifying a length and would result in a length of 255.  I'm leaving those alone for now.
 
+-- UMICH: -- unclear if necessary, since size of column is unclear
+
 alter table osp_guidance modify site_id varchar2(99);
 alter table osp_review modify site_id varchar2(99);
 alter table osp_style modify site_id varchar2(99);
@@ -19,13 +26,19 @@ alter table osp_presentation modify site_id varchar2(99);
 alter table osp_presentation_layout modify site_id varchar2(99);
 alter table osp_wizard modify site_id varchar2(99);
 
+--------------------------------------------------------------------------------
 
 -- SAK-5595
 -- http://bugs.sakaiproject.org/jira/browse/SAK-5595
 -- Conversion script error: missing column DURATION in SAM_MEDIA_T
 
+-- UMICH: Need this.
+
 alter table SAM_MEDIA_T add (DURATION varchar(36)); 
 
+---------------------------------------------------------------------------------
+
+-- UMICH: need these (where are the rest of the metaobj permissions?)
 -- OSP-1289
 -- http://bugs.osportfolio.org/jira/browse/OSP-1289
 -- Need to add delete to the default metaobj permissions
@@ -33,6 +46,10 @@ INSERT INTO SAKAI_REALM_FUNCTION VALUES (SAKAI_REALM_FUNCTION_SEQ.NEXTVAL, 'meta
 INSERT INTO SAKAI_REALM_RL_FN VALUES((select REALM_KEY from SAKAI_REALM where REALM_ID = '!site.template'), (select ROLE_KEY from SAKAI_REALM_ROLE where ROLE_NAME = 'maintain'), (select FUNCTION_KEY from SAKAI_REALM_FUNCTION where FUNCTION_NAME = 'metaobj.delete'));
 INSERT INTO SAKAI_REALM_RL_FN VALUES((select REALM_KEY from SAKAI_REALM where REALM_ID = '!site.template.course'), (select ROLE_KEY from SAKAI_REALM_ROLE where ROLE_NAME = 'Instructor'), (select FUNCTION_KEY from SAKAI_REALM_FUNCTION where FUNCTION_NAME = 'metaobj.delete'));
 INSERT INTO SAKAI_REALM_RL_FN VALUES((select REALM_KEY from SAKAI_REALM where REALM_ID = '/site/mercury'), (select ROLE_KEY from SAKAI_REALM_ROLE where ROLE_NAME = 'maintain'), (select FUNCTION_KEY from SAKAI_REALM_FUNCTION where FUNCTION_NAME = 'metaobj.delete'));
+
+----------------------------------------------------------------------------------
+
+-- UMICH: need this
 
 -- SAK-5564
 -- http://bugs.sakaiproject.org/jira/browse/SAK-5564
