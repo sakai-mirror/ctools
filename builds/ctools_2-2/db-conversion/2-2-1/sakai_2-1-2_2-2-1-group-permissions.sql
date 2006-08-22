7c3d4850ad1bc3708ecb28ad4cb08870ac911bbf
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-08-22 19:14:10 +0000

    CTools: add corrected version of the 2.1.2 -> 2.2.0 group permission update sql.
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@13910 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ builds/ctools_2-2/db-conversion/2-2-1/sakai_2-1-2_2-2-1-group-permissions.sql
@@ -0,0 +1,55 @@
+---------------------------------------------------------------------------------------------------------------
+-- backfill new site permissions into existing site realms
+---------------------------------------------------------------------------------------------------------------
+
+
+
+-- for each realm that has a grant of the 'annc.all.groups', we add these others:
+CREATE TABLE PERMISSIONS_TEMP (FUNCTION_KEY INTEGER);
+INSERT INTO PERMISSIONS_TEMP VALUES ((SELECT FUNCTION_KEY FROM SAKAI_REALM_FUNCTION WHERE FUNCTION_NAME ='asn.all.groups'));
+INSERT INTO PERMISSIONS_TEMP VALUES ((SELECT FUNCTION_KEY FROM SAKAI_REALM_FUNCTION WHERE FUNCTION_NAME ='calendar.all.groups'));
+INSERT INTO PERMISSIONS_TEMP VALUES ((SELECT FUNCTION_KEY FROM SAKAI_REALM_FUNCTION WHERE FUNCTION_NAME ='content.all.groups'));
+
+
+-- this is wrong, since the join will prevent anything from being found (the function name isn't in the temp table),
+-- so use the following one.
+
+-- INSERT INTO SAKAI_REALM_RL_FN (REALM_KEY, ROLE_KEY, FUNCTION_KEY)
+-- SELECT
+--     SRRF.REALM_KEY, SRRF.ROLE_KEY, TMP.FUNCTION_KEY
+-- FROM
+--     SAKAI_REALM_RL_FN SRRF
+--     JOIN PERMISSIONS_TEMP TMP ON (SRRF.FUNCTION_KEY = TMP.FUNCTION_KEY)
+--     WHERE SRRF.FUNCTION_KEY = (SELECT FUNCTION_KEY FROM SAKAI_REALM_FUNCTION SRF WHERE SRF.FUNCTION_NAME ='annc.all.groups')
+--     AND NOT EXISTS (
+--         SELECT 1
+--             FROM SAKAI_REALM_RL_FN SRRFI
+--             WHERE SRRFI.REALM_KEY=SRRF.REALM_KEY AND SRRFI.ROLE_KEY=SRRF.ROLE_KEY AND SRRFI.FUNCTION_KEY=TMP.FUNCTION_KEY
+--     );
+
+-- sample
+-- -- construct tuples for update 
+-- -- based on function key and realm  Realm is just to restrict the size of the query
+-- -- and should be deleted when do for real.
+-- insert  into SAKAI_REALM_RL_FN (REALM_KEY, ROLE_KEY, FUNCTION_KEY)
+-- select SRRF.REALM_KEY, SRRF.ROLE_KEY, TMP.FUNCTION_KEY
+-- from  SAKAI_REALM_RL_FN SRRF, DLH_PERMISSIONS_TEMP TMP
+-- where SRRF.FUNCTION_KEY = 169 and SRRF.REALM_KEY=76769  
+-- AND NOT EXISTS (
+--         SELECT 1
+--             FROM SAKAI_REALM_RL_FN SRRFI
+--             WHERE SRRFI.REALM_KEY=SRRF.REALM_KEY AND SRRFI.ROLE_KEY=SRRF.ROLE_KEY AND SRRFI.FUNCTION_KEY=TMP.FUNCTION_KEY)
+
+insert  into SAKAI_REALM_RL_FN (REALM_KEY, ROLE_KEY, FUNCTION_KEY)
+select SRRF.REALM_KEY, SRRF.ROLE_KEY, TMP.FUNCTION_KEY
+from  SAKAI_REALM_RL_FN SRRF, PERMISSIONS_TEMP TMP
+where SRRF.FUNCTION_KEY = (SELECT FUNCTION_KEY FROM SAKAI_REALM_FUNCTION SRF WHERE SRF.FUNCTION_NAME ='annc.all.groups')
+AND NOT EXISTS (
+        SELECT 1
+            FROM SAKAI_REALM_RL_FN SRRFI
+            WHERE SRRFI.REALM_KEY=SRRF.REALM_KEY AND SRRFI.ROLE_KEY=SRRF.ROLE_KEY AND SRRFI.FUNCTION_KEY=TMP.FUNCTION_KEY)
+
+
+DROP TABLE PERMISSIONS_TEMP;
+
+
