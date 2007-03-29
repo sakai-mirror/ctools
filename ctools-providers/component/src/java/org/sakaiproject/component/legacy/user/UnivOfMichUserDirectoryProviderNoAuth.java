5edcf5bad5d113e7b009ed5bf7ccbfe06fb1742f
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-03-29 19:17:00 +0000

    CTools: copy ctools-providers to the 2.4 branch
    git-svn-id: https://source.sakaiproject.org/svn/ctools/branches/ctools_2-4@28159 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ ctools-providers/component/src/java/org/sakaiproject/component/legacy/user/UnivOfMichUserDirectoryProviderNoAuth.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.sakaiproject.component.legacy.user;
+
+/**
+ * @author dlhaines
+ *
+ */
+public class UnivOfMichUserDirectoryProviderNoAuth extends
+		UnivOfMichUserDirectoryProvider {
+
+	/**
+	 * 
+	 */
+ 	protected void initTestMode() {
+ 		TEST_MODE = true;
+ 	}
+
+}
