df841323d0a4433cff4ca477839a97177fd66446
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-05-23 19:44:55 +0000

    CTools: add junit test framework for UMich CM.
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@30975 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ ctools-providers/component/impl/test/org/sakaiproject/coursemanagement/impl/CourseManagementServiceUnivOfMichImplTest.java
@@ -0,0 +1,219 @@
+package org.sakaiproject.coursemanagement.impl;
+
+import junit.framework.TestCase;
+
+public class CourseManagementServiceUnivOfMichImplTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testInit() {
+		fail("Not yet implemented");
+	}
+
+	public void testDestroy() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCourseSet() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetChildCourseSets() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCourseSets() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCourseSetMemberships() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCanonicalCourse() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetEquivalentCanonicalCourses() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCanonicalCourses() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetAcademicSessions() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCurrentAcademicSessions() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetAcademicSession() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCourseOffering() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCourseOfferingsInCourseSet() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetEquivalentCourseOfferings() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCourseOfferingMemberships() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetSection() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetSections() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetChildSections() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetSectionMemberships() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetEnrollmentSet() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetEnrollmentSets() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetEnrollments() {
+		fail("Not yet implemented");
+	}
+
+	public void testIsEnrolledStringSetOfString() {
+		fail("Not yet implemented");
+	}
+
+	public void testIsEnrolledStringString() {
+		fail("Not yet implemented");
+	}
+
+	public void testFindEnrollment() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetInstructorsOfRecordIds() {
+		fail("Not yet implemented");
+	}
+
+	public void testFindCurrentlyEnrolledEnrollmentSets() {
+		fail("Not yet implemented");
+	}
+
+	public void testFindCurrentlyInstructingEnrollmentSets() {
+		fail("Not yet implemented");
+	}
+
+	public void testFindInstructingSectionsString() {
+		fail("Not yet implemented");
+	}
+
+	public void testFindEnrolledSections() {
+		fail("Not yet implemented");
+	}
+
+	public void testFindInstructingSectionsStringString() {
+		fail("Not yet implemented");
+	}
+
+	public void testFindCourseOfferings() {
+		fail("Not yet implemented");
+	}
+
+	public void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	public void testFindCourseSets() {
+		fail("Not yet implemented");
+	}
+
+	public void testFindCourseOfferingRoles() {
+		fail("Not yet implemented");
+	}
+
+	public void testFindCourseSetRoles() {
+		fail("Not yet implemented");
+	}
+
+	public void testFindSectionRoles() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetCourseOfferingsInCanonicalCourse() {
+		fail("Not yet implemented");
+	}
+
+	public void testIsAcademicSessionDefined() {
+		fail("Not yet implemented");
+	}
+
+	public void testIsCanonicalCourseDefined() {
+		fail("Not yet implemented");
+	}
+
+	public void testIsCourseOfferingDefined() {
+		fail("Not yet implemented");
+	}
+
+	public void testIsCourseSetDefined() {
+		fail("Not yet implemented");
+	}
+
+	public void testIsEnrollmentSetDefined() {
+		fail("Not yet implemented");
+	}
+
+	public void testIsSectionDefined() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetSectionCategories() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetSectionCategoryDescription() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetEnrollmentStatusDescriptions() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetGradingSchemeDescriptions() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetMembershipStatusDescriptions() {
+		fail("Not yet implemented");
+	}
+
+	public void testUnpackId() {
+		fail("Not yet implemented");
+	}
+
+}
