7feadb9f9c304f03d2ba0da7f5fc787877fe762f
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-05-31 19:22:59 +0000

    CTools: add sectionField provider to ctools.
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@31262 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ ctools-providers/component/impl/test/org/sakaiproject/sitemanage/impl/SectionFieldManagerImplTest.java
@@ -0,0 +1,29 @@
+package org.sakaiproject.sitemanage.impl;
+
+import junit.framework.TestCase;
+
+public class SectionFieldManagerImplTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testDummy() {
+		assertTrue("placeholder test",1==1);
+	}
+	
+	/*
+	public void testGetRequiredFields() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetSectionEid() {
+		fail("Not yet implemented");
+	}
+	*/
+
+}
