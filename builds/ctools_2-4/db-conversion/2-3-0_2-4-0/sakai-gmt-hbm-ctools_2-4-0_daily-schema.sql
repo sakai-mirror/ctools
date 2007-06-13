efa5d9888871825191ccbce4c59e27b2af0ef4f5
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-06-13 19:32:00 +0000

    CTools: update per Drew with clearer constraint names.
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@31581 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- builds/ctools_2-4/db-conversion/2-3-0_2-4-0/sakai-gmt-hbm-ctools_2-4-0_daily-schema.sql
+++ builds/ctools_2-4/db-conversion/2-3-0_2-4-0/sakai-gmt-hbm-ctools_2-4-0_daily-schema.sql
@@ -3,11 +3,24 @@ drop table GMT_GOAL cascade constraints;
 drop table GMT_GOALSET cascade constraints;
 drop table GMT_LINK cascade constraints;
 drop table GMT_RATING cascade constraints;
-create table GMT_CONTEXT_ASSOCIATION (FROM_CONTEXT varchar2(99 char) not null, TO_CONTEXT varchar2(99 char) not null, VERSION number(10,0) not null, primary key (FROM_CONTEXT, TO_CONTEXT));
-create table GMT_GOAL (ID varchar2(36 char) not null, VERSION number(10,0) not null, TITLE varchar2(255 char), DESCRIPTION clob, GOALSET_ID varchar2(36 char) not null, PARENT_GOAL_ID varchar2(36 char), primary key (ID));
-create table GMT_GOALSET (ID varchar2(36 char) not null, VERSION number(10,0) not null, SUGGESTED_BY varchar2(36 char), TITLE varchar2(255 char), DESCRIPTION clob, CONTEXT varchar2(99 char) not null, PUBLISHED number(1,0) not null, primary key (ID));
-create table GMT_LINK (ID varchar2(36 char) not null, VERSION number(10,0) not null, ACTIVITY_REF varchar2(255 char) not null, GOAL_ID varchar2(36 char) not null, RUBRIC clob, RATIONALE clob, EXPORT_STRING number(10,0) not null, VISIBLE number(1,0) not null, primary key (ID), unique (ACTIVITY_REF, GOAL_ID));
-create table GMT_RATING (ID varchar2(36 char) not null, VERSION number(10,0) not null, ITEM_REF varchar2(255 char) not null, LINK_ID varchar2(36 char) not null, RATING varchar2(255 char) not null, COMMENTS clob, primary key (ID), unique (ITEM_REF, LINK_ID));
+
+-- These are changes suggested by Drew to give the constraints more appropriate names.
+
+create table GMT_CONTEXT_ASSOCIATION (FROM_CONTEXT varchar2(99 char) not null, TO_CONTEXT varchar2(99 char) not null, VERSION number(10,0) not null, constraint PK_GMT_CONTEXT_ASSOCIATION primary key (FROM_CONTEXT, TO_CONTEXT));
+
+create table GMT_GOAL (ID varchar2(36 char) not null, VERSION number(10,0) not null, TITLE varchar2(255 char), DESCRIPTION clob, GOALSET_ID varchar2(36 char) not null, PARENT_GOAL_ID varchar2(36 char), constraint PK_GMT_GOAL primary key (ID));
+
+create table GMT_GOALSET (ID varchar2(36 char) not null, VERSION number(10,0) not null, SUGGESTED_BY varchar2(36 char), TITLE varchar2(255 char), DESCRIPTION clob, CONTEXT varchar2(99 char) not null, PUBLISHED number(1,0) not null, constraint PK_GMT_GOALSET primary key (ID));
+
+create table GMT_LINK (ID varchar2(36 char) not null, VERSION number(10,0) not null, ACTIVITY_REF varchar2(255 char) not null, GOAL_ID varchar2(36 char) not null, RUBRIC clob, RATIONALE clob, EXPORT_STRING number(10,0) not null, VISIBLE number(1,0) not null, constraint PK_GMT_LINK primary key (ID), constraint UK_GMT_LINK unique (ACTIVITY_REF, GOAL_ID));
+
+create table GMT_RATING (ID varchar2(36 char) not null, VERSION number(10,0) not null, ITEM_REF varchar2(255 char) not null, LINK_ID varchar2(36 char) not null, RATING varchar2(255 char) not null, COMMENTS clob, constraint PK_GMT_RATING primary key (ID), constraint UK_GMT_RATING unique (ITEM_REF, LINK_ID));
+
+-- create table GMT_CONTEXT_ASSOCIATION (FROM_CONTEXT varchar2(99 char) not null, TO_CONTEXT varchar2(99 char) not null, VERSION number(10,0) not null, primary key (FROM_CONTEXT, TO_CONTEXT));
+-- create table GMT_GOAL (ID varchar2(36 char) not null, VERSION number(10,0) not null, TITLE varchar2(255 char), DESCRIPTION clob, GOALSET_ID varchar2(36 char) not null, PARENT_GOAL_ID varchar2(36 char), primary key (ID));
+-- create table GMT_GOALSET (ID varchar2(36 char) not null, VERSION number(10,0) not null, SUGGESTED_BY varchar2(36 char), TITLE varchar2(255 char), DESCRIPTION clob, CONTEXT varchar2(99 char) not null, PUBLISHED number(1,0) not null, primary key (ID));
+-- create table GMT_LINK (ID varchar2(36 char) not null, VERSION number(10,0) not null, ACTIVITY_REF varchar2(255 char) not null, GOAL_ID varchar2(36 char) not null, RUBRIC clob, RATIONALE clob, EXPORT_STRING number(10,0) not null, VISIBLE number(1,0) not null, primary key (ID), unique (ACTIVITY_REF, GOAL_ID));
+-- create table GMT_RATING (ID varchar2(36 char) not null, VERSION number(10,0) not null, ITEM_REF varchar2(255 char) not null, LINK_ID varchar2(36 char) not null, RATING varchar2(255 char) not null, COMMENTS clob, primary key (ID), unique (ITEM_REF, LINK_ID));
 alter table GMT_GOAL add constraint FK4D66888484568B1C foreign key (GOALSET_ID) references GMT_GOALSET;
 alter table GMT_GOAL add constraint FK4D6688846E3F0D23 foreign key (PARENT_GOAL_ID) references GMT_GOAL;
 alter table GMT_LINK add constraint FK4D68B96B5F1AA7B8 foreign key (GOAL_ID) references GMT_GOAL;
