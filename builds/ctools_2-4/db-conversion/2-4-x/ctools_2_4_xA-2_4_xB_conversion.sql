a07ec63f88bd7ceeacd9699c40c6cc104d368ced
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-07-25 18:38:24 +0000

    CTools: update documentation for 2.4.xA to 2.4.xB conversion."
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@33139 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- builds/ctools_2-4/db-conversion/2-4-x/ctools_2_4_xA-2_4_xB_conversion.sql
+++ builds/ctools_2-4/db-conversion/2-4-x/ctools_2_4_xA-2_4_xB_conversion.sql
@@ -1,21 +1,14 @@
 -- Convert db from the UMich 2.4.xA to 2.4.xB.
 
--- $HeadURL:$
--- $Id:$
+-- $HeadURL$
+-- $Id$
 
-
-
-
--- These are changes from trunk.
+-- SAK-9725 changes for a patch from trunk
+prompt SAK-9725 changes
 -- From revision 30105 of $S/reference/trunk/docs/conversion/sakai_2_4_0-2_5_0_oracle_conversion.sql
 
-prompt SAK-9725 changes
--- This is the Oracle Sakai 2.4.0 (or later) -> 2.5.0 conversion script
 ----------------------------------------------------------------------------------------------------------------------------------------
---
--- use this to convert a Sakai database from 2.4.0 to 2.5.0.  Run this before you run your first app server.
 -- auto.ddl does not need to be enabled in your app server - this script takes care of all new TABLEs, changed TABLEs, and changed data.
---
 ----------------------------------------------------------------------------------------------------------------------------------------
 
 --metaobj conversion
@@ -24,6 +17,7 @@ alter TABLE metaobj_form_def add alternateViewXslt varchar2(36) NULL;
 
 -- end SAK-9725
 
+-- This is for change in 2.4.x
 prompt SAK-9808
 -- SAK-9808: Implement ability to delete threaded messages within Forums
 alter table MFR_MESSAGE_T add DELETED number(1, 0) default '0' not null;
@@ -43,3 +37,6 @@ create index MFR_MESSAGE_DELETED_I on MFR_MESSAGE_T (DELETED);
 -- create index SAM_SECTION_ASSESSMENTID_I on SAM_SECTION_T (ASSESSMENTID);
 -- create index SAM_SECTIONMETA_SECTIONID_I on SAM_SECTIONMETADATA_T (SECTIONID);
 
+commit;
+
+-- end
