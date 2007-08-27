e7d888d35e3330c67713904048ca5dbd1dc62896
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-08-27 23:59:39 +0000

    CTools: add db conversion script for 2.4.xG SAK 10163, SAK 11204.
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@34484 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ builds/ctools_2-4/db-conversion/2-4-x/ctools_2-4-xF_2-4-xG.sql
@@ -0,0 +1,24 @@
+-- $HeadURL:$
+-- $Id:$
+
+--Chat SAK-10163
+ ALTER TABLE CHAT2_CHANNEL ADD PLACEMENT_ID varchar2(99) NULL;
+ ALTER TABLE CHAT2_CHANNEL RENAME COLUMN contextDefaultChannel TO placementDefaultChannel;
+	 
+ update CHAT2_CHANNEL cc
+ set cc.PLACEMENT_ID = (select st.TOOL_ID from SAKAI_SITE_TOOL st where st.REGISTRATION = 'sakai.chat'
+    and cc.placementDefaultChannel = 1
+    and cc.CONTEXT = st.SITE_ID )
+ where EXISTS
+ (select st.TOOL_ID from SAKAI_SITE_TOOL st where st.REGISTRATION = 'sakai.chat'
+    and cc.placementDefaultChannel = 1
+    and cc.CONTEXT = st.SITE_ID)
+
+-- SAK-11204 	  	 
+-- Force this to happen and report problems bu having a non failing SQL statement first
+SELECT 1 FROM DUAL;
+
+ALTER TABLE CALENDAR_EVENT ADD COLUMN(RANGE_START INTEGER), COLUMN(RANGE_END INTEGER);
+
+CREATE INDEX CALENDAR_EVENT_RSTART ON CALENDAR_EVENT(RANGE_START);
+CREATE INDEX CALENDAR_EVENT_REND ON CALENDAR_EVENT(RANGE_END);
