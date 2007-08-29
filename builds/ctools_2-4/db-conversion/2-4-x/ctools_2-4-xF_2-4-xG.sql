16151be3be53f3e510ab88180ff50ec4927428ae
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-08-29 19:58:01 +0000

    CTools: update keywords and conversion sql for chat2 conversion to multiple chat rooms: SAK 10163
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@34548 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- builds/ctools_2-4/db-conversion/2-4-x/ctools_2-4-xF_2-4-xG.sql
+++ builds/ctools_2-4/db-conversion/2-4-x/ctools_2-4-xF_2-4-xG.sql
@@ -1,18 +1,29 @@
--- $HeadURL:$
--- $Id:$
+-- $HeadURL$
+-- $Id$
 
 --Chat SAK-10163
  ALTER TABLE CHAT2_CHANNEL ADD PLACEMENT_ID varchar2(99) NULL;
  ALTER TABLE CHAT2_CHANNEL RENAME COLUMN contextDefaultChannel TO placementDefaultChannel;
 	 
- update CHAT2_CHANNEL cc
- set cc.PLACEMENT_ID = (select st.TOOL_ID from SAKAI_SITE_TOOL st where st.REGISTRATION = 'sakai.chat'
-    and cc.placementDefaultChannel = 1
-    and cc.CONTEXT = st.SITE_ID )
- where EXISTS
- (select st.TOOL_ID from SAKAI_SITE_TOOL st where st.REGISTRATION = 'sakai.chat'
-    and cc.placementDefaultChannel = 1
-    and cc.CONTEXT = st.SITE_ID)
+--  update CHAT2_CHANNEL cc
+--  set cc.PLACEMENT_ID = (select st.TOOL_ID from SAKAI_SITE_TOOL st where st.REGISTRATION = 'sakai.chat'
+--     and cc.placementDefaultChannel = 1
+--     and cc.CONTEXT = st.SITE_ID )
+--  where EXISTS
+--  (select st.TOOL_ID from SAKAI_SITE_TOOL st where st.REGISTRATION = 'sakai.chat'
+--     and cc.placementDefaultChannel = 1
+--     and cc.CONTEXT = st.SITE_ID)
+
+update CHAT2_CHANNEL cc
+   set cc.PLACEMENT_ID = (select st.TOOL_ID from SAKAI_SITE_TOOL st where st.REGISTRATION = 'sakai.chat'
+      and cc.placementDefaultChannel = 1
+      and cc.CONTEXT = st.SITE_ID 
+      and ROWNUM = 1 )
+   where EXISTS
+   (select st.TOOL_ID from SAKAI_SITE_TOOL st where st.REGISTRATION = 'sakai.chat'
+      and cc.placementDefaultChannel = 1
+      and cc.CONTEXT = st.SITE_ID 
+      and ROWNUM = 1);
 
 -- SAK-11204 	  	 
 -- Force this to happen and report problems bu having a non failing SQL statement first
