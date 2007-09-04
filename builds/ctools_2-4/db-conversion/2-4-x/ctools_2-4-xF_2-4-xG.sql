d9eba4de893dee60e32107151867e64505441ef4
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-09-04 20:13:13 +0000

    CTools: retract calendar sql.  It must be done by code.
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@34677 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- builds/ctools_2-4/db-conversion/2-4-x/ctools_2-4-xF_2-4-xG.sql
+++ builds/ctools_2-4/db-conversion/2-4-x/ctools_2-4-xF_2-4-xG.sql
@@ -26,10 +26,14 @@ update CHAT2_CHANNEL cc
       and ROWNUM = 1);
 
 -- SAK-11204 	  	 
--- Force this to happen and report problems bu having a non failing SQL statement first
-SELECT 1 FROM DUAL;
 
-ALTER TABLE CALENDAR_EVENT ADD COLUMN(RANGE_START INTEGER), COLUMN(RANGE_END INTEGER);
+-- code requires this to be done by server, not in advance.  That means that the indices
+-- must be moved afterwards by hand.
 
-CREATE INDEX CALENDAR_EVENT_RSTART ON CALENDAR_EVENT(RANGE_START);
-CREATE INDEX CALENDAR_EVENT_REND ON CALENDAR_EVENT(RANGE_END);
+-- Force this to happen and report problems but having a non failing SQL statement first
+-- SELECT 1 FROM DUAL;
+
+-- ALTER TABLE CALENDAR_EVENT ADD COLUMN(RANGE_START INTEGER), COLUMN(RANGE_END INTEGER);
+
+-- CREATE INDEX CALENDAR_EVENT_RSTART ON CALENDAR_EVENT(RANGE_START);
+-- CREATE INDEX CALENDAR_EVENT_REND ON CALENDAR_EVENT(RANGE_END);
