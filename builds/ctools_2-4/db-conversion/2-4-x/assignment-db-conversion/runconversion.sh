af20710004e291e4cecd13564fe62df39d701482
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2007-11-28 20:51:50 +0000

    CTools: initial version of the assignment db conversion scripts for 2.4.xQ
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@38842 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ builds/ctools_2-4/db-conversion/2-4-x/assignment-db-conversion/runconversion.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+CLASSPATH="$CLASSPATH:$CATALINA_HOME/common/lib/sakai-util-log-2-4-x.jar"
+CLASSPATH="$CLASSPATH:$CATALINA_HOME/common/lib/log4j-1.2.8.jar"
+CLASSPATH="$CLASSPATH:$CATALINA_HOME/components/sakai-util-pack/WEB-INF/lib/sakai-util-2-4-x.jar"
+CLASSPATH="$CLASSPATH:$CATALINA_HOME/components/sakai-web-pack/WEB-INF/lib/sakai-entity-util-2-4-x.jar"
+CLASSPATH="$CLASSPATH:$CATALINA_HOME/components/sakai-assignment-pack/WEB-INF/lib/sakai-assignment-impl-2-4-x.jar"
+CLASSPATH="$CLASSPATH:$CATALINA_HOME/shared/lib/commons-logging-1.0.4.jar"
+#CLASSPATH="$CLASSPATH:$CATALINA_HOME/shared/lib/commons-dbcp-1.2.1.jar"
+CLASSPATH="$CLASSPATH:$CATALINA_HOME/shared/lib/commons-pool-1.3.jar"
+CLASSPATH="$CLASSPATH:$CATALINA_HOME/shared/lib/sakai-util-api-2-4-x.jar"
+CLASSPATH="$CLASSPATH:$CATALINA_HOME/shared/lib/sakai-entity-api-2-4-x.jar"
+CLASSPATH="$CLASSPATH:$CATALINA_HOME/shared/lib/sakai-assignment-api-2-4-x.jar"
+CLASSPATH="$CLASSPATH:$CATALINA_HOME/shared/lib/commons-dbcp-1.2.1.jar"
+CLASSPATH="$CLASSPATH:$CATALINA_HOME/shared/lib/commons-collections-3.1.jar"
+
+##### JDBC DRIVER #####
+## MYSQL ##
+CLASSPATH="$CLASSPATH:$CATALINA_HOME/common/lib/mysql-connector-java-5.1.5-bin.jar"
+## ORACLE ##
+CLASSPATH="$CLASSPATH:$CATALINA_HOME/common/lib/ojdbc-14.jar"
+
+java $JAVA_OPTS  \
+      -classpath "$CLASSPATH" \
+	org.sakaiproject.assignment.impl.conversion.impl.UpgradeSchema "$@" 
