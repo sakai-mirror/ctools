eec01e5329a8e66a1adcff324a3fd75788691ebd
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-01-25 18:45:04 +0000

    CTools: update 2.4.x S and R for SAK 9996.  Extend the externalsLint to tell the difference between modules versions.
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@40542 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- builds/ctools_2-4/configs/ctools_2-4-x_CANDIDATE/externalsLint.pl
+++ builds/ctools_2-4/configs/ctools_2-4-x_CANDIDATE/externalsLint.pl
@@ -8,9 +8,17 @@
 # The line below will find duplicate module definitions.
 # externalsLint.pl ctools_2-4-xS.externals  | sort | uniq -c | grep -v '  1'
 
+# This will produce a list of what modules are different between two externals files.
+# externalsLint.pl ctools_2-4-xR.externals ctools_2-4-xS.externals | sort | uniq -c | fgrep -v ' 2'
+
+$MODULE_DEFS=0;
+$REVISIONS=1;
+
 while(<>) {
   next if (/^\s*#/ | /^\s*$/);
-  ($module) = m|\s*(\S+)\s|;
-  print "$module\n";
+  ($module,$revision) = m|\s*(\S+)\s+-r(\d+)\s|;
+  print "$module\n" if ($MODULE_DEFS);
+  print "$module $revision\n" if ($REVISIONS);
 }
+
 #end
