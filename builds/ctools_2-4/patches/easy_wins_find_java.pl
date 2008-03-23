3a9a8a25d28d82027db1ef50ae3dde4c37c3cd36
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-03-23 21:18:38 +0000

    CTools: customize easy_wins_find_java.pl for CTools.
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@42987 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- builds/ctools_2-4/patches/easy_wins_find_java.pl
+++ builds/ctools_2-4/patches/easy_wins_find_java.pl
@@ -6,13 +6,22 @@ use File::Find;
 # Then does the easy quick win static bugs
 # Tested only on Linux
 # Feb 4 2008
-# Last modified: March 6th 2008
+#
+# $HeadURL:$
+# $Id:$
 #
 # Contact Alan Berg: a.m.berg@uva.nl
 
+# Adapted and generalized by David Haines (dlhaines@Umich.edu).
+# Tracked for CTools in CT-462.
+
 # Can Change
-my $src="/home/aberg/apps/sakai_fluth_remover/trunk/src/"; # Source directory
-my $DEBUG=0; # If 0 or less then modify source code. If greater than zero then just output to stdout what will be done 
+#my $src="/home/aberg/apps/sakai_fluth_remover/trunk/src/"; # Source directory
+#my $DEBUG=0; # If 0 or less then modify source code. If greater than zero then just output to stdout what will be done 
+
+# Set these up with default values, then override from command line if appropriate.
+my ($src,$DEBUG) = (".",0);
+($src,$DEBUG) = @ARGV;
 
 # Internal use only
 my @file_list=();
@@ -21,7 +30,7 @@ my $global_change=0;
 # MAIN
 #
 unless (-d $src){
-    print "Please change \$src variable to point to source directory";
+    print "Please change \$src variable to point to source directory src: [$src]\n";
 }
 
 find(\&list_java, $src);
@@ -42,9 +51,13 @@ foreach $file (@file_list){
         $content.=$line;
 
         unless(length($line)==length($temp_line)){
+	  chomp($temp_line);
+	  chomp($line);
             print "$file [$line_no]\n";
-            print $line;
-            print "$temp_line\n";
+            print "replace [$temp_line]\n with [$line]\n";
+
+#            print $line;
+#            print "$temp_line\n";
             $global_change++;
             $flag=1;
         }
