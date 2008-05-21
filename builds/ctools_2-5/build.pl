ff7d68c0538638f3da01212418d4dfafc600fee6
jonespm@umich.edu <jonespm@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-05-21 20:49:18 +0000

    CTools: Changes to remove parts of sakai from the ctools build easier, also improvenets to build script.
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@46623 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- builds/ctools_2-5/build.pl
+++ builds/ctools_2-5/build.pl
@@ -3,20 +3,30 @@
 # Script to test manual builds of ctools
 # $HeadURL:
 # $Id: 
+# TODO: Modularize this file, it's starting to get out of control
+#
 #http://www.perl.com/doc/FAQs/FAQ/oldfaq-html/Q5.15.html
 #http://www.unix.org.ua/orelly/perl/prog3/ch09_04.htm
 
+use strict;
+use warnings;
+
+use File::Path;
 use File::Basename;
 use FindBin qw($Bin);
 use Term::ANSIColor qw(:constants);
 use POSIX qw(strftime);
-#use Properties;
-
-use strict;
+use IO::File;
+#Config::Properties is not part of core perl so its installed after
+use lib "$Bin/lib";
+use Config::Properties;
 
 #Define which type to build, this might be prompted later
 my $typevar = "prod";
+#Define the default properties file to look for
+my $defaultpropertyfile = "defaultbuild.properties";
 
+#Set some environment variables for java and maven
 $ENV{'JAVA_OPTS'} = "-server -Xms512m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=196m -XX:NewSize=192m -XX:MaxNewSize=384m";
 $ENV{'MAVEN_OPTS'} = "-Xms256m -Xmx512m";
 
@@ -61,48 +71,81 @@ my %requiredSoftware = (
 
 ); 
 
+#Reads a configuration file from a specified build directory and returns a Config::Properties object with the results
+sub readConfig {
+    my %args = @_;
+    my $builddir = $args{'builddir'};
+    #Read in the default build file
+    my ($defaultproperties,$ctoolsproperties,$ctoolsfile, $fh);
+    $fh = new IO::File();
+    $defaultproperties = new Config::Properties();
+    $ctoolsproperties = new Config::Properties();
+    $fh->open("$builddir/$defaultpropertyfile","r") or die "unable to find file: $!\n";
+    eval {
+        $defaultproperties->load($fh);
+    };
+    if ($@) {
+	return $@;
+    }
+    $fh->close();
+    if ($ctoolsfile = $defaultproperties->getProperty('ctoolsBuildFileName')) {
+	print "This property file detected as: $ctoolsfile\n";
+	$fh->open("$builddir/$ctoolsfile","r") or die "unable to find file: $!\n";
+	eval {
+	    $ctoolsproperties->load($fh);
+	};
+	if ($@) {
+	    return $@;
+	}
+	$fh->close();
+    }
+    return $ctoolsproperties;
+}
+
+sub checkVersions {
 #Check Versions
-my ($majorerror,$minorerror) = 0;
-my ($required,$result);
-my ($minmajor,$minminor,$maxmajor,$maxminor,$resultmajor,$resultminor);
-my ($minstatus,$maxstatus);
+    my ($majorerror,$minorerror) = 0;
+    my ($required,$result);
+    my ($minmajor,$minminor,$maxmajor,$maxminor,$resultmajor,$resultminor);
+    my ($minstatus,$maxstatus);
 
 #Check for required software
-for $required ( keys %requiredSoftware) {
-    print "Checking program $required: ";
-    $result=`$requiredSoftware{$required}{cmd} 2>&1`;
-    chop $result;
-    ($minmajor,$minminor) = split(/\./,$requiredSoftware{$required}{min});
-    ($maxmajor,$maxminor) = split(/\./,$requiredSoftware{$required}{max});
-    if ($result) {
-        ($resultmajor,$resultminor) = $result =~ m/(\d+)\.(\d+)\.\d+/;
-	$minstatus = ($resultmajor >= $minmajor && $resultminor >= $minminor) ? "Ok" : "version installed lower than tested";
-	$maxstatus = ($resultmajor <= $maxmajor && $resultminor <= $maxminor) ? "Ok" : "version installed higher than tested";
+    for $required (keys %requiredSoftware) {
+	print "Checking program $required: ";
+	$result=`$requiredSoftware{$required}{cmd} 2>&1`;
+	chop $result;
+	($minmajor,$minminor) = split(/\./,$requiredSoftware{$required}{min});
+	($maxmajor,$maxminor) = split(/\./,$requiredSoftware{$required}{max});
+	if ($result) {
+	    ($resultmajor,$resultminor) = $result =~ m/(\d+)\.(\d+)\.\d+/;
+	    $minstatus = ($resultmajor >= $minmajor && $resultminor >= $minminor) ? "Ok" : "version installed lower than tested";
+	    $maxstatus = ($resultmajor <= $maxmajor && $resultminor <= $maxminor) ? "Ok" : "version installed higher than tested";
 
-	if ($minstatus != "Ok" || $maxstatus ne "Ok") {
-	    $minorerror = 1;
+	    if ($minstatus ne "Ok" || $maxstatus ne "Ok") {
+		$minorerror = 1;
+	    }
+
+	    print "\n  Version detected as: $resultmajor.$resultminor\n";
+	    print "  Minimum Version tested: $minmajor.$minminor $minstatus\n";
+	    print "  Maximum Version tested: $maxmajor.$maxminor $maxstatus\n";
+
 	}
-	
-         print "\n  Version detected as: $resultmajor.$resultminor\n";
-	 print "  Minimum Version tested: $minmajor.$minminor $minstatus\n";
-	 print "  Maximum Version tested: $maxmajor.$maxminor $maxstatus\n";
-	
-	}
-    else {
-	print "Not found, please install version $minmajor.$minminor - $maxmajor.$maxminor\n";
-	$majorerror=1;
-    } 
+	else {
+	    print "Not found, please install version $minmajor.$minminor - $maxmajor.$maxminor\n";
+	    $majorerror=1;
+	} 
+    }
+
+    if ($majorerror) {
+	die "Please install the necessary programs before continuing!\n";
+    }
+
+    if ($minorerror) {
+	print "The software was not tested with this version, you can continue but may have errors.\n";
+    }
 }
 
-if ($majorerror) {
-    print "Please install the necessary programs before continuing!\n";
-    exit();
-}
-
-if ($minorerror) {
-    print "The software was not tested with this version, you can continue but may have errors.\n";
-}
-
+checkVersions();
 print "Getting newest version of config files from svn:\n";
 my ($svninfo,$svnversion,$svnvar);
 $svninfo = `svn up`;
@@ -117,10 +160,10 @@ if ($svnversion) {
 
 print "Searching for valid build files!\n";
 #Look for a list of builds in the config directory
-my @builds = `find configs -name "defaultbuild.properties" | sort`;
+my @builds = `find configs -name "$defaultpropertyfile" | sort`;
 if (!@builds) {
      #Look for a list of builds in the current directory
-     @builds = `find . -name "defaultbuild.properties" | sort`;
+     @builds = `find . -name "$defaultpropertyfile" | sort`;
 }
 
 print "Detected ".@builds." candidate(s) (You can create a new candidate by cp -r one of the configs in the configs directory and editing the sakai.tag property to match this new name.):\n";
@@ -139,12 +182,22 @@ do
     $buildin = <STDIN>;	# Get input
     chop $buildin;	    # Chop off newline
 }
-until ($builddir = dirname($builds[$buildin]));	    # Redo while wrong input
+until ($buildin =~ m/\d+/ && $buildin >=0 && $buildin < $buildcount); # Redo while wrong input
 
+$builddir = dirname($builds[$buildin]);
+
+my $ctoolsproperties = readConfig(builddir=>$builddir);
+if (ref($ctoolsproperties) ne "Config::Properties") {
+    print "There was a problem reading the property file specified, please correct before building.\n";
+    die $ctoolsproperties;
+
+}
+
+my ($sakaitag,$logdir);
+$sakaitag = $ctoolsproperties->getProperty('sakai.tag');
 #Find out other options like if they want to clean out the build, or just update whats there
-#ETC!
 
-print "NOTE: The resulting packages will be placed in $Bin/artifacts/$builddir\n";
+print "NOTE: The resulting packages will be placed in:\n$Bin/artifacts/$builddir\n";
 
 #Prompt if they want a clean install, parts of this are still being worked on in the scripts.
 do
@@ -157,9 +210,15 @@ until ($cleanin eq "n" || $cleanin eq "y");	    # Redo while wrong input
 
 #Get a log file started for further analysis
 my $nowstring = strftime "%m-%d-%y_%H-%M-%S", localtime;
-mkdir("./logs");
-open(STDOUT, "| tee ./logs/outputlog.$nowstring.log");
 
+#I would like to put this in the work directory, but it's always cleaned up after the log is written
+$logdir = "$Bin/logs";
+eval{mkpath ($logdir)};
+if ($@) {
+    die "Unable to create $logdir: $@";
+}
+
+open(STDOUT, "| tee $logdir/outputlog.$nowstring.log");
 my $cleanvar;
 #Command to start it up!
 if ($cleanin eq "n") {
@@ -173,7 +232,6 @@ else {
 chdir($builddir);
 my $cmd = "ant -s build.xml -Dtype=$typevar $cleanvar $svnvar 2>&1";
 system $cmd; 
-close(STDOUT);
 
 0;
 
