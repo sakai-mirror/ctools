50a1a049729807bfde3ac7f1c4bc4b939de1dc45
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-08-13 15:09:07 +0000

    CTools: script cleanup.
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@50662 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- ctools-webservices/src/sash/groovy/listSites.groovy
+++ ctools-webservices/src/sash/groovy/listSites.groovy
@@ -2,29 +2,32 @@
 // $HeadURL$
 // $Id$
 
+// Get the component manager to allow looking up the required services;
 import org.sakaiproject.component.cover.ComponentManager
+
+// Will need to use the site service.
 import org.sakaiproject.site.api.SiteService
+def siteService = ComponentManager.get("org.sakaiproject.site.api.SiteService");
 
+// keep some statistics
+def start = System.currentTimeMillis();
+def  siteCnt = 0;
+println "start time: "+start
 
-start = System.currentTimeMillis()
-  println "start time: "+start
-
-  siteCnt = 0;
-
-siteService = ComponentManager.get("org.sakaiproject.site.api.SiteService");
+// for each site print some information
 siteService.getSites(SiteService.SelectionType.NON_USER, ["course","project"], null, null, null,null).each 
 { 
-  siteCnt++
-    println  "title: [${it.title}], id: [${it.id}]"
+  siteCnt++;
+  println  "title: [${it.title}], id: [${it.id}]";
 }
 //     {site ->
 // 	    println "site: ".site;
 //     }
 
-  def stop = System.currentTimeMillis()
-  println "end time: " + stop
-  def elapsed = stop -start
-  println "for "+siteCnt+" sites the elapsed time is: "+elapsed
-  println "time per site: "+elapsed / siteCnt
+def stop = System.currentTimeMillis();
+println "end time: " + stop;
+def elapsed = stop -start;
+println "for "+siteCnt+" sites the elapsed time is: "+elapsed;
+println "time per site: "+elapsed / siteCnt + " ms";
 
   // end
