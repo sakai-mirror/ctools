5786c9d2a1b6b0fe97117373042b55131e9c8b41
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-08-13 15:43:52 +0000

    CTools: test sleep / flush / and svn keyword subsitition.
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@50665 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- ctools-webservices/src/sash/groovy/listSites.groovy
+++ ctools-webservices/src/sash/groovy/listSites.groovy
@@ -2,6 +2,8 @@
 // $HeadURL$
 // $Id$
 
+def sleepTime=5000;
+def version = "$Id:$";
 // Get the component manager to allow looking up the required services;
 import org.sakaiproject.component.cover.ComponentManager
 
@@ -9,14 +11,19 @@ import org.sakaiproject.component.cover.ComponentManager
 import org.sakaiproject.site.api.SiteService
 def siteService = ComponentManager.get("org.sakaiproject.site.api.SiteService");
 
+println version;
+println "sleep time: "+sleepTime;
+
 // keep some statistics
 def start = System.currentTimeMillis();
 def  siteCnt = 0;
-println "start time: "+start
+println "start time: "+start;
+out.flush();
 
 // for each site print some information
 siteService.getSites(SiteService.SelectionType.NON_USER, ["course","project"], null, null, null,null).each 
 { site ->
+  Thread.sleep(sleepTime);
   siteCnt++;
   println  "title: [${site.title}], id: [${site.id}]";
   //  println  "title: [${it.title}], id: [${it.id}]";
