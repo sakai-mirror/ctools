878566a2d46e5f9e63f3bf541d10089fbe59cf5b
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-08-13 15:01:14 +0000

    CTools: add basic metrics to the listSite script.
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@50661 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- ctools-webservices/src/sash/groovy/listSites.groovy
+++ ctools-webservices/src/sash/groovy/listSites.groovy
@@ -1,16 +1,30 @@
 // List all sites
-// $HeadURL:$
-// $Id:$
+// $HeadURL$
+// $Id$
 
 import org.sakaiproject.component.cover.ComponentManager
 import org.sakaiproject.site.api.SiteService
 
-    siteService = ComponentManager.get("org.sakaiproject.site.api.SiteService");
-    siteService.getSites(SiteService.SelectionType.NON_USER, ["course","project"], null, null, null,null).each 
-    { println  "title: [${it.title}], id: [${it.id}]"
-	    }
+
+start = System.currentTimeMillis()
+  println "start time: "+start
+
+  siteCnt = 0;
+
+siteService = ComponentManager.get("org.sakaiproject.site.api.SiteService");
+siteService.getSites(SiteService.SelectionType.NON_USER, ["course","project"], null, null, null,null).each 
+{ 
+  siteCnt++
+    println  "title: [${it.title}], id: [${it.id}]"
+}
 //     {site ->
 // 	    println "site: ".site;
 //     }
 
-// end
+  def stop = System.currentTimeMillis()
+  println "end time: " + stop
+  def elapsed = stop -start
+  println "for "+siteCnt+" sites the elapsed time is: "+elapsed
+  println "time per site: "+elapsed / siteCnt
+
+  // end
