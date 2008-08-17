4b3a3e5f96b28cfda75594e7681d8ccb780ea3db
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-08-17 19:17:33 +0000

    CTools: clean up test for db access and reading properties.
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@50826 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- ctools-webservices/src/sash/groovy/testDbSql.groovy
+++ ctools-webservices/src/sash/groovy/testDbSql.groovy
@@ -1,42 +1,26 @@
 // $HeadURL$
 // $Id$
 
-// checking comment: works!
-
-/*
-  Test to see if can read properties.
- */
-
-
-
-// def myURL = "jdbc:oracle:thin:@localhost:12439:SAKAIDEV";
-// def user = "dlhaines";
-// def password = "dlhaines";
+// checkin comment: reading properties works
 
 Properties properties  = new Properties();
 FileInputStream props = new FileInputStream("test.properties");
 properties.load(props);
 props.close();
 
+/* print all the properties read in.
 properties.each {
   println it;
 }
+*/
 
 def candidateSitesSql = "select * from (select distinct SITE_ID from SAKAI_SITE_TOOL where SITE_ID like '~%'and SITE_ID not in (select SITE_ID from SAKAI_SITE_TOOL where REGISTRATION = 'sakai.rsf.evaluation') order by SITE_ID) where rownum < 3"
 
-
-// ds = new oracle.jdbc.pool.OracleDataSource();
-// ds.setURL(myURL);
-// conn = ds.getConnection(user, password);
-// conn.execute(candidateSitesSql);
-
   import groovy.sql.Sql;
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import javax.sql.DataSource;
 
-//def sql = Sql.newInstance(myURL, user, password,"oracle.jdbc.driver.OracleDriver");
-def sql = Sql.newInstance(properties.myURL, properties.user, properties.password,"oracle.jdbc.driver.OracleDriver");
+def sql = Sql.newInstance(properties.myURL, properties.user, 
+			  properties.password,properties.dbdriver);
+
 sql.eachRow(candidateSitesSql) { println it.SITE_ID }
 
 // end
