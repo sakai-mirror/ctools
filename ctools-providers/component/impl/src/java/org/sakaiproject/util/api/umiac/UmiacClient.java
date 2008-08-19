377fb4e75abb08381d9e8150893d7ffb1101c024
rwellis@umich.edu <rwellis@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-08-19 15:10:08 +0000

    CT-623 finish cleaning up Umiac code
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@50902 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- ctools-providers/component/impl/src/java/org/sakaiproject/util/api/umiac/UmiacClient.java
+++ ctools-providers/component/impl/src/java/org/sakaiproject/util/api/umiac/UmiacClient.java
@@ -71,25 +71,6 @@ public interface UmiacClient {
 	 * @exception IdUnusedException If there is no group by this id.
 	 */
 	public abstract Map<String, String> getGroupRoles(String id) throws IdUnusedException; // getGroupRoles
-	
-	/**
-	 * Get all the members of a group, by their group role for Course Evaluations
-	 * 
-	 * A behind-the-scenes modification of getGroupRoles(String id) tailored to
-	 * the Evaluation tool.
-	 *  
-	 * Instructors are pulled from a table that may be modified in M-Pathways by E&E
-	 * or departmental staff to determine who has "be.evaluated" permission in the
-	 * Evaluation tool rather than getting that information from the course roster.
-	 * The students should be the same as for getGroupRoles(String id).
-	 * 
-	 * @see org.sakaiproject.component.legacy.realm.EvalGroupsProviderUnivOfMichImpl
-	 * 
-	 * @param id The group id followed by "|" and the evaluation eid
-	 * @return The group's full membership information
-	 * @exception IdUnusedException If there is no group by this id.
-	 */
-	public abstract Map<String, String> getEvalGroupRoles(String[] id) throws IdUnusedException;  // variant of getGroupRoles for Course Evaluations
 
 	/**
 	 * Get all the external realm ids the user has a role in, and the role.  
@@ -100,24 +81,6 @@ public interface UmiacClient {
 	 */
 	public abstract Map<String, String> getUserSections(String id); // getUserSections
 	
-	/**
-	 * Get all the external group ids the user has a role in, and the role, for Course Evaluations.
-	 * 
-	 * A behind-the-scenes modification of getUserSections(String id) tailored to
-	 * the Evaluation tool.
-	 * 
-	 * 	 * Instructors are pulled from a table that may be modified in M-Pathways by E&E
-	 * or departmental staff to determine who has "be.evaluated" permission in the
-	 * Evaluation tool rather than getting that information from the course roster.
-	 * The students should be the same as for getGroupRoles(String id).
-	 * 
-	 * @see org.sakaiproject.component.legacy.realm.EvalGroupsProviderUnivOfMichImpl
-	 * 
-	 * @param id The user id.
-	 * @return A map of the group id to the role for this user.
-	 */
-	public abstract Map<String, String> getEvalUserSections(String id); // variant of getUserSections for Course Evaluations
-
 	/**
 	 * Get all the members of a group, by their group role.
 	 * A group is defined by multiple external ids.
