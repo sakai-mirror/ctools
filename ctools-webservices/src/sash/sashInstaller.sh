bdb2174eca0d312b586509540fba23258e6610f5
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-08-25 19:48:43 +0000

    CTools: add script to package up sash so it can be installed.
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@51366 66ffb92e-73f9-0310-93c1-f5514f145a0a

---
+++ ctools-webservices/src/sash/sashInstaller.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Package up a tar file suitable to install this into an existing Sakai deployment.
+# $HeadURL:$
+# $Id:$
+
+# steps
+# - extract and compile sash
+# - deploy to a new, empty, deploy directory
+# - package that up.
+# - state how to deploy.
+
+DEPLOYED=$HOME/deploy
+TAR_NAME=sash-0.1.1
+tar -z -f $TAR_NAME.tgz -C $DEPLOYED -c .
+
+#end
