a3c39e9e3146a435912e8e56b7096bd18fab15df
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-09-02 00:17:54 +0000

    CTools: ensure code demo works.
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@51698 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- ctools-webservices/src/sash/groovy/TestArgs.groovy
+++ ctools-webservices/src/sash/groovy/TestArgs.groovy
@@ -1,18 +1,47 @@
-// $HeadURL:$
-// $Id:$
+// $HeadURL$
+// $Id$
 
+
+//  Argument passing to a script (without an explicit class) works.
+// println args;
+// args.each{println "[${it}]"}
+
+
+// With sash 0.1.1 argument passing to a script with a main routine in a class doesn't seem to work.
+// so to pass the args into the class can use:
+println "started at: ${new Date()}";
+new TestArgsX(args).perform();
 
 //def main(String args[]) {
 //}
 
 // // works !!, can get args into the class
-// println args;
-// args.each{println "[${it}]"}
-// new TestArgsX(args);
 
-println args;
-args.each{println "[${it}]"}
-new TestArgsX(args).perform();
+// println args;
+//args.each{println "[${it}]"}
+// new TestArgsX(args).perform();
+
+// with a class like
+ class TestArgsX {
+
+   def ar ;
+
+   // constructor will copy the args directly from the script.
+   TestArgsX(String[] a) {
+     println "TestArgsX created at: ${new Date()}";
+     ar = a;
+     a.each{println "a: ${it}"}
+     ar.each{println "ar: ${it}"}
+   }
+
+   // the script is started by explicitly calling a routine to take the
+   // place of main.
+   void perform() {
+     println "args: ar:";
+     ar.each{println "[${it}]"};
+   }
+ }
+
 
 
 // empty args
@@ -41,21 +70,21 @@ new TestArgsX(args).perform();
 // run = new TestArgsX(args);
 // run.perform();
 
- class TestArgsX {
+//  class TestArgsX {
 
-   def ar ;
+//    def ar ;
 
-   TestArgsX(String[] a) {
-     ar = a;
-     a.each{println "a: ${it}"}
-     ar.each{println "ar: ${it}"}
-   }
+//    TestArgsX(String[] a) {
+//      ar = a;
+//      a.each{println "a: ${it}"}
+//      ar.each{println "ar: ${it}"}
+//    }
 
-   void perform() {
-     println "args";
-     ar.each{println "[${it}]"};
-   }
- }
+//    void perform() {
+//      println "args";
+//      ar.each{println "[${it}]"};
+//    }
+//  }
 // //      public static void main(String[] args) {  
 // //   //    public static void main() {  
 // //       println "hello from groovy ${new Date()}";
