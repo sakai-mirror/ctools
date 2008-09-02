d6bcd32bbaaad258bb606f293a54916505fffafd
jimeng@umich.edu <jimeng@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-09-02 17:37:12 +0000

    TXB-87 TXB-64 TXB-32 Improvements to ctools textbook provider and its build process. Getting textbook data for all courses (including those with no textbook data)
    git-svn-id: https://source.sakaiproject.org/svn/ctools/branches/CT-616@51706 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- ctools-providers/component/impl/src/java/org/sakaiproject/tbook/provider/impl/UnivOfMichTextbookProvider.java
+++ ctools-providers/component/impl/src/java/org/sakaiproject/tbook/provider/impl/UnivOfMichTextbookProvider.java
@@ -29,6 +29,7 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 
 import net.sf.json.JSONArray;
+import net.sf.json.JSONException;
 import net.sf.json.JSONObject;
 
 import org.apache.commons.logging.Log;
@@ -79,7 +80,11 @@ public class UnivOfMichTextbookProvider implements TextbookProvider
 		Course course = null;
 		
 		JSONObject jsonObject = getJsonObject(courseId);
-		if(jsonObject != null)
+		if(jsonObject == null || jsonObject.isNullObject() || jsonObject.isEmpty())
+		{
+			// skip this one
+		}
+		else
 		{
 			course = getCourseFromJsonObject(jsonObject);
 		}
@@ -118,7 +123,11 @@ public class UnivOfMichTextbookProvider implements TextbookProvider
 					for(String jsonString : jsonStrings)
 					{
 						JSONObject jsonObject = JSONObject.fromObject(jsonString);
-						if(jsonObject != null)
+						if(jsonObject == null || jsonObject.isNullObject() || jsonObject.isEmpty())
+						{
+							// skip this one
+						}
+						else
 						{
 							Course course = this.getCourseFromJsonObject(jsonObject);
 							if(course != null)
@@ -202,246 +211,282 @@ public class UnivOfMichTextbookProvider implements TextbookProvider
 	{
 		Course course = new Course();
 		
-		String year = jsonObject.getString("year");
-		int textbook_item_number = jsonObject.getInt("textbook_item_number");
-		String campus = jsonObject.getString("campus");
-		String subject = jsonObject.getString("subject");
-		String catalog_nbr = jsonObject.getString("catalog_nbr");
-		String term = jsonObject.getString("term");
-		String class_section = jsonObject.getString("class_section");
-		
-		String courseId = year + "," + term + "," + campus + "," + subject + "," + catalog_nbr + "," + class_section;
-		course.setCourseId(courseId );
-		
-		String title = subject + "-" + catalog_nbr + " " + class_section;
-		course.setTitle(title );
-		
 		try
 		{
-			String coursepack_location = jsonObject.getString("coursepack_location");
-			course.setCoursepackLocation(coursepack_location);
-		}
-		catch(Exception e)
-		{
-			// ignore -- no coursepack location
-		}
-		
-		String course_comment = null;
-		
-		try
-		{
-			course_comment = jsonObject.getString("textbook_note");
-		}
-		catch (Exception e)
-		{
-			// ignore -- let comment remain null
-		}
-		
-		if(course_comment == null)
-		{
-			course_comment = "";
-		}
-		course_comment = course_comment.trim();
-		
-		JSONArray required_items = jsonObject.getJSONArray("required_items");
-		if(required_items == null)
-		{
-			course_comment += "\n" + "No textbooks required for this course";
-		}
-		else
-		{
-			SortedMap<String,CourseItem> itemMap = new TreeMap<String, CourseItem>();
-			for(int i = 0; i < required_items.size(); i++)
+			String year = jsonObject.getString("year");
+			String campus = jsonObject.getString("campus");
+			String subject = jsonObject.getString("subject");
+			String catalog_nbr = jsonObject.getString("catalog_nbr");
+			String term = jsonObject.getString("term");
+			String class_section = jsonObject.getString("class_section");
+			
+			String courseId = year + "," + term + "," + campus + "," + subject + "," + catalog_nbr + "," + class_section;
+			course.setCourseId(courseId );
+			
+			//int textbook_item_number = jsonObject.getInt("textbook_item_number");
+			try
 			{
-				CourseItem newItem = null;
-				String sortStr = "";
-				JSONObject item = required_items.getJSONObject(i);
-				
-				String item_type = item.getString("item_type");
-				if(item_type != null && item_type.trim().toLowerCase().equals("book"))
+				String same_as = jsonObject.getString("same_as");
+			}
+			catch(Exception e)
+			{
+				// ignore
+			}
+			String title = subject + "-" + catalog_nbr + " " + class_section;
+			try
+			{
+				String class_title = jsonObject.getString("class_title");
+				if(class_title != null && ! class_title.trim().equals(""))
 				{
-					sortStr += "0";
+					title += " - " + class_title;
 				}
-				else
+			}
+			catch(Exception e)
+			{
+				// ignore
+			}
+			course.setTitle(title );
+			try
+			{
+				String has_books = jsonObject.getString("has_books");
+			}
+			catch(Exception e)
+			{
+				// ignore
+			}
+			
+			
+			try
+			{
+				String coursepack_location = jsonObject.getString("coursepack_location");
+				course.setCoursepackLocation(coursepack_location);
+			}
+			catch(Exception e)
+			{
+				// ignore -- no coursepack location
+			}
+			
+			String course_comment = null;
+			
+			try
+			{
+				course_comment = jsonObject.getString("textbook_note");
+			}
+			catch (Exception e)
+			{
+				// ignore -- let comment remain null
+			}
+			
+			if(course_comment == null)
+			{
+				course_comment = "";
+			}
+			course_comment = course_comment.trim();
+			
+			JSONArray required_items = jsonObject.getJSONArray("required_items");
+			if(required_items == null)
+			{
+				course_comment += "\n" + "No textbooks required for this course";
+			}
+			else
+			{
+				SortedMap<String,CourseItem> itemMap = new TreeMap<String, CourseItem>();
+				for(int i = 0; i < required_items.size(); i++)
 				{
-					sortStr += "1";
-				}
-
-				String item_comment = null;
-				try
-				{
-					item_comment = item.getString("comments");
-				}
-				catch(Exception e)
-				{
-					// ignore -- let comments remain null
-				}
-				CourseBookStatus status = CourseBookStatus.REQUIRED;
-				if(item.has("required"))
-				{
-					try
-					{
-						String statusStr = item.getString("required");
-						if(statusStr == null)
-						{
-							// ignore -- assume it's required
-							sortStr += "0";
-						}
-						else if(statusStr.trim().toLowerCase().equals("rec"))
-						{
-							status = CourseBookStatus.RECOMMENDED;
-							sortStr += "1";
-						}
-						else if(statusStr.trim().toLowerCase().equals("opt"))
-						{
-							status = CourseBookStatus.OPTIONAL;
-							sortStr += "2";
-						}
-						else if(statusStr.trim().toLowerCase().equals("avd"))
-						{
-							status = CourseBookStatus.AVOID;
-							sortStr += "3";
-						}
-						else
-						{
-							// ignore -- assume it's required
-							sortStr += "0";
-						}
-					}
-					catch (Exception e)
-					{
-						// ignore -- assume it's required
-						sortStr += "0";
-					}
-				}
-				else
-				{
-					// ignore -- assume it's required
-					sortStr += "0";
+					CourseItem newItem = null;
+					String sortStr = "";
+					JSONObject item = required_items.getJSONObject(i);
 					
-				}
-				
-				boolean late_use = false;
-				try
-				{
-					late_use = item.getBoolean("late_use");
-				}
-				catch(Exception e)
-				{
-					// ignore -- let "late_use" remain false 
-				}
-				if(late_use)
-				{
-					sortStr += "1";
-				}
-				else
-				{
-					sortStr += "0";
-				}
-
-				if(item_type != null && item_type.trim().toLowerCase().equals("book"))
-				{
-					String isbn = item.getString("isbn");
-					List<Book> bookmatches = logic.getBooks(isbn);
-					if(bookmatches == null || bookmatches.isEmpty())
+					String item_type = item.getString("item_type");
+					if(item_type != null && item_type.trim().toLowerCase().equals("book"))
 					{
-						// need to log this as an invalid book?
-						logger.warn("Could not find book (ISBN: " + isbn + ") for course: ");
-						continue;
+						sortStr += "0";
 					}
 					else
 					{
-						newItem = new CourseBook();
-						CourseBook newBook = (CourseBook) newItem;
-						
-						Book book = bookmatches.get(0);
-						newBook.setIsbn(book.getIsbn());
-						
-						boolean reserve_requested = false;
+						sortStr += "1";
+					}
+	
+					String item_comment = null;
+					try
+					{
+						item_comment = item.getString("comments");
+					}
+					catch(Exception e)
+					{
+						// ignore -- let comments remain null
+					}
+					CourseBookStatus status = CourseBookStatus.REQUIRED;
+					if(item.has("required"))
+					{
 						try
 						{
-							reserve_requested = item.getBoolean("on_reserve");
-						}
-						catch(Exception e)
-						{
-							// ignore -- let reserve_requested remain false
-						}
-						newBook.setLibraryReserveRequested(reserve_requested);
-						
-						String preferredBookseller = null;
-						try
-						{
-							preferredBookseller = item.getString("preferred_vendor");
-							if(preferredBookseller == null || preferredBookseller.trim().equals("") || preferredBookseller.trim().toLowerCase().equals("null"))
+							String statusStr = item.getString("required");
+							if(statusStr == null)
 							{
-								preferredBookseller = null;
+								// ignore -- assume it's required
+								sortStr += "0";
+							}
+							else if(statusStr.trim().toLowerCase().equals("rec"))
+							{
+								status = CourseBookStatus.RECOMMENDED;
+								sortStr += "1";
+							}
+							else if(statusStr.trim().toLowerCase().equals("opt"))
+							{
+								status = CourseBookStatus.OPTIONAL;
+								sortStr += "2";
+							}
+							else if(statusStr.trim().toLowerCase().equals("avd"))
+							{
+								status = CourseBookStatus.AVOID;
+								sortStr += "3";
+							}
+							else
+							{
+								// ignore -- assume it's required
+								sortStr += "0";
 							}
 						}
-						catch(Exception e)
+						catch (Exception e)
 						{
-							// ignore -- let preferredBookseller remain null
+							// ignore -- assume it's required
+							sortStr += "0";
 						}
-						newBook.setPreferredBookseller(preferredBookseller);
-						
-						BigDecimal retailPrice = null;
-						String val = null;
-						try
-						{
-							val = item.getString("msrp");
-							retailPrice = new BigDecimal(val);
-						}
-						catch(Exception e)
-						{
-							// ignore -- let price remain null
-							logger.debug("Problem reading price: " + val + " --> " + e);
-						}
-						newBook.setRetailPrice(retailPrice);
-						//course.addBook(book, item_comment, status, late_use, reserve_requested, preferredBookseller, retailPrice);
 					}
-				}
-				else
-				{
-					newItem = new CourseMaterials();
-					CourseMaterials newMaterials = (CourseMaterials) newItem;
-					
-					String description = item.getString("description");
-					newMaterials.setDescription(description);
-					
-					//course.addMaterials(description, item_comment, status, late_use);
-				}
-				if(newItem == null)
-				{
-					continue;
-				}
-				newItem.setLateUse(late_use);
-				newItem.setNotes(item_comment);
-				newItem.setStatus(status);
-				newItem.setCourse(course);
-				
-				if(item.has("seqnum"))
-				{
-					String seqnum = item.getString("seqnum");
-					if(seqnum != null)
+					else
 					{
-						sortStr += seqnum.trim();
+						// ignore -- assume it's required
+						sortStr += "0";
+						
+					}
+					
+					boolean late_use = false;
+					try
+					{
+						late_use = item.getBoolean("late_use");
+					}
+					catch(Exception e)
+					{
+						// ignore -- let "late_use" remain false 
+					}
+					if(late_use)
+					{
+						sortStr += "1";
+					}
+					else
+					{
+						sortStr += "0";
+					}
+	
+					if(item_type != null && item_type.trim().toLowerCase().equals("book"))
+					{
+						String isbn = item.getString("isbn");
+						List<Book> bookmatches = logic.getBooks(isbn);
+						if(bookmatches == null || bookmatches.isEmpty())
+						{
+							// need to log this as an invalid book?
+							logger.warn("Could not find book (ISBN: " + isbn + ") for course: ");
+							continue;
+						}
+						else
+						{
+							newItem = new CourseBook();
+							CourseBook newBook = (CourseBook) newItem;
+							
+							Book book = bookmatches.get(0);
+							newBook.setIsbn(book.getIsbn());
+							
+							boolean reserve_requested = false;
+							try
+							{
+								reserve_requested = item.getBoolean("on_reserve");
+							}
+							catch(Exception e)
+							{
+								// ignore -- let reserve_requested remain false
+							}
+							newBook.setLibraryReserveRequested(reserve_requested);
+							
+							String preferredBookseller = null;
+							try
+							{
+								preferredBookseller = item.getString("preferred_vendor");
+								if(preferredBookseller == null || preferredBookseller.trim().equals("") || preferredBookseller.trim().toLowerCase().equals("null"))
+								{
+									preferredBookseller = null;
+								}
+							}
+							catch(Exception e)
+							{
+								// ignore -- let preferredBookseller remain null
+							}
+							newBook.setPreferredBookseller(preferredBookseller);
+							
+							BigDecimal retailPrice = null;
+							String val = null;
+							try
+							{
+								val = item.getString("msrp");
+								retailPrice = new BigDecimal(val);
+							}
+							catch(Exception e)
+							{
+								// ignore -- let price remain null
+								logger.debug("Problem reading price: " + val + " --> " + e);
+							}
+							newBook.setRetailPrice(retailPrice);
+							//course.addBook(book, item_comment, status, late_use, reserve_requested, preferredBookseller, retailPrice);
+						}
+					}
+					else
+					{
+						newItem = new CourseMaterials();
+						CourseMaterials newMaterials = (CourseMaterials) newItem;
+						
+						String description = item.getString("description");
+						newMaterials.setDescription(description);
+						
+						//course.addMaterials(description, item_comment, status, late_use);
+					}
+					if(newItem == null)
+					{
+						continue;
+					}
+					newItem.setLateUse(late_use);
+					newItem.setNotes(item_comment);
+					newItem.setStatus(status);
+					newItem.setCourse(course);
+					
+					if(item.has("seqnum"))
+					{
+						String seqnum = item.getString("seqnum");
+						if(seqnum != null)
+						{
+							sortStr += seqnum.trim();
+						}
+					}
+					itemMap.put(sortStr, newItem);
+				}
+				for(String key : itemMap.keySet())
+				{
+					CourseItem newItem = itemMap.get(key);
+					if(newItem instanceof CourseBook)
+					{
+						course.addBook((CourseBook) newItem);
+					}
+					else // if(newItem instanceof CourseMaterials)
+					{
+						course.addMaterials((CourseMaterials) newItem);
 					}
 				}
-				itemMap.put(sortStr, newItem);
 			}
-			for(String key : itemMap.keySet())
-			{
-				CourseItem newItem = itemMap.get(key);
-				if(newItem instanceof CourseBook)
-				{
-					course.addBook((CourseBook) newItem);
-				}
-				else // if(newItem instanceof CourseMaterials)
-				{
-					course.addMaterials((CourseMaterials) newItem);
-				}
-			}
+			course.setComment(course_comment);
 		}
-		course.setComment(course_comment);
+		catch(JSONException e)
+		{
+			logger.warn("Exception parsing JSON Object:\n" + jsonObject, e);
+		}
 		return course;
 	}
 
