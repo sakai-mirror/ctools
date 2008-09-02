8e6aa74176d4affed3da634f613d907e1e0e726f
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-09-02 00:26:15 +0000

    CTools: Update groovy test args documents.
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@51699 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- ctools-webservices/src/sash/groovy/TestArgs.groovy
+++ ctools-webservices/src/sash/groovy/TestArgs.groovy
@@ -6,22 +6,16 @@
 // println args;
 // args.each{println "[${it}]"}
 
+println "started at: ${new Date()}";
 
 // With sash 0.1.1 argument passing to a script with a main routine in a class doesn't seem to work.
-// so to pass the args into the class can use:
-println "started at: ${new Date()}";
+// To get around this pass the args into the class constructor that is called
+// explicitly.  Also invoke the main part of the class explicitly.
+
+// salt away the args and start processing
 new TestArgsX(args).perform();
 
-//def main(String args[]) {
-//}
-
-// // works !!, can get args into the class
-
-// println args;
-//args.each{println "[${it}]"}
-// new TestArgsX(args).perform();
-
-// with a class like
+// The class is structured like this:
  class TestArgsX {
 
    def ar ;
@@ -44,67 +38,4 @@ new TestArgsX(args).perform();
 
 
 
-// empty args
-// class TestArgs {
-
-//    public static void main(String[] a) {  
-//      println "hello from groovy ${new Date()}";
-//      println a;
-//      a.each{println "[${it}]"};
-//    }
-
-//  }
-
-// get error that args is not defined.
-// class TestArgs {
-
-//     public static void main(String[] a) {  
-//       println "hello from groovy ${new Date()}";
-//       println args;
-//       args.each{println "[${it}]"};
-//     }
-//   }
-
-//def  args;
-
-// run = new TestArgsX(args);
-// run.perform();
-
-//  class TestArgsX {
-
-//    def ar ;
-
-//    TestArgsX(String[] a) {
-//      ar = a;
-//      a.each{println "a: ${it}"}
-//      ar.each{println "ar: ${it}"}
-//    }
-
-//    void perform() {
-//      println "args";
-//      ar.each{println "[${it}]"};
-//    }
-//  }
-// //      public static void main(String[] args) {  
-// //   //    public static void main() {  
-// //       println "hello from groovy ${new Date()}";
-// //       println args;
-// //       args.each{println "[${it}]"};
-// //     }
-//   }
-
-
-// Prints to log, but doesn't notice the args.
-// change name from Driver to TestArgs doesn't help.
-// class Driver {
-
-//   static void main(String[] args) {  
-//     println "hello from groovy ${new Date()}";
-//     args.each{println "[${it}]"};
-//   }
-
-// }
-
-// Works under sash
-//println "hello from groovy ${new Date()}";
-//#args.each{println "[${it}]"};
+#end
