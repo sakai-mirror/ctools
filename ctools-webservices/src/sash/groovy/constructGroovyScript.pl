3ee894844be076db2d458bf4176ea10c4d487db9
dlhaines@umich.edu <dlhaines@umich.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2008-09-03 16:05:14 +0000

    CTools: clean up debugging.  bracket the included files in output file.
    git-svn-id: https://source.sakaiproject.org/svn/ctools/trunk@51752 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- ctools-webservices/src/sash/groovy/constructGroovyScript.pl
+++ ctools-webservices/src/sash/groovy/constructGroovyScript.pl
@@ -1,27 +1,27 @@
 #!/usr/bin/env perl -n
+# $HeadURL:$
+# $Id:$
+
 print "// groovy script from template on ".`date`;
 while(<>) {
   $includeFile = undef();
   chomp;
-#  print "l: [$_]\n";
   $line = $_;
-#  print "line: [$line]\n";
   if ($line =~ m/^\s*include:\s*(.+)/) {
-#    print "found match\n";
     $includeFile = $1;
   }
-#  print "1: [$1]\n";
-#  print "if: [$includeFile]\n";
   includeFile($includeFile) if ($includeFile);
-  print "*** $_\n"  unless ($includeFile);
+  print "$_\n"  unless ($includeFile);
 }
 
 sub includeFile{
   my($fileName) = shift;
-  print "** include file: [$fileName]\n";
+  # print "** include file: [$fileName]\n";
+  print "// ******** include file: [$fileName]\n";
   open(FH, "< $fileName") or die("$fileName: $!");
   while(<FH>) {
     print;
   }
+  print "// ******** end of included file: [$fileName]\n";
 }
 # end
